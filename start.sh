d0b8879430a8f9719bff77b1d00de32e2cae576a
Максим <56961609+PhoenixInDarkness@users.noreply.github.com>
2019-10-24 15:35:49 +0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Tweak nginx to match the workers to cpu's
+procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
+
+# Start supervisord and services
+exec supervisord -c /etc/supervisord.conf
